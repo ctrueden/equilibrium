df0241da2ab97a348e277a9c3c2564462322939b
Curtis Rueden <ctrueden@wisc.edu>
2022-11-05 17:35:02 -0500

    Fix pinterest link extraction

--- _bin/pinterest.sh
+++ _bin/pinterest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while [ $# -gt 0 ]
 do
-  pin=$(grep -oh 'related-pins:[0-9]*' -- "$1" | sed 's/^related-pins://')
+  pin=$(grep -oh 'data-savepage-href="ios-app://[0-9]*/pinterest/pin/[0-9]*' "$1" | sed 's_.*/\([0-9]*\)$_\1_')
   img=$(grep -oh 'https://i.pinimg.com/originals/[^"]*' -- "$1" | head -n1)
   test "$pin" -a "$img" &&
     echo "https://www.pinterest.com/pin/$pin/|$img" ||
