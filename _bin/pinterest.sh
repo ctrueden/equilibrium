eac5f2602cb0fc06988548a323c910ca64d1e9b3
Curtis Rueden <ctrueden@wisc.edu>
2024-01-15 21:02:03 -0600

    Make pinterest script work again
    It seems they changed from originals to 736x across the board.
    
    The originals image still exists (though you have to guess whether each
    one is a jpg or a png), but it's not referenced anymore in the HTML.

--- _bin/pinterest.sh
+++ _bin/pinterest.sh
@@ -2,7 +2,7 @@
 while [ $# -gt 0 ]
 do
   pin=$(grep -oh 'data-savepage-href="ios-app://[0-9]*/pinterest/pin/[0-9]*' "$1" | sed 's_.*/\([0-9]*\)$_\1_')
-  img=$(grep -oh 'https://i.pinimg.com/originals/[^"]*' -- "$1" | head -n1)
+  img=$(grep -oh 'https://i.pinimg.com/\(originals\|736x\)/[^"]*' -- "$1" | head -n1)
   test "$pin" -a "$img" &&
     echo "https://www.pinterest.com/pin/$pin/|$img" ||
     echo "[FAIL] $1"
