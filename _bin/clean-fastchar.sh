e2d0cc676d90b16492979562150f2c83a4293351
Curtis Rueden <ctrueden@wisc.edu>
2024-11-02 22:19:36 -0500

    clean-fastchar.sh: support filenames with spaces

--- _bin/clean-fastchar.sh
+++ _bin/clean-fastchar.sh
@@ -1,8 +1,10 @@
 # This script cleans up fastcharacter.com-generated character sheets
 # saved as HTML documents, so that they can be embedded in other websites.
 
-for arg in $@
+while [ $# -gt 0 ]
 do
+  arg=$1
+  shift
   perl -0777 -i \
     -pe 's/\n<script.*\n<\/script>//igs;' \
     -pe 's/\n<div id="charsheet02">.*<\/body>/\n<\/div>\n<\/body>/igs' \
