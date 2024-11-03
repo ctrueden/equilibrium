4207b2ac9644834ca8159ecb6b36d4196a3a3c53
Curtis Rueden <ctrueden@wisc.edu>
2024-11-03 17:16:09 -0600

    clean-fastchar.sh: fix to work with new output

--- _bin/clean-fastchar.sh
+++ _bin/clean-fastchar.sh
@@ -6,7 +6,8 @@ do
   arg=$1
   shift
   perl -0777 -i \
-    -pe 's/\n<script.*\n<\/script>//igs;' \
-    -pe 's/\n<div id="charsheet02">.*<\/body>/\n<\/div>\n<\/body>/igs' \
+    -pe 's/\n<script[^\n]*<\/script>//igs;' \
+    -pe 's/\n<script id="savepage-shadowloader".*\n<\/script>//igs;' \
+    -pe 's/<a name="footer"><\/a>\n<div id="footer">.*<\/body>/\n<\/div>\n<\/body>/igs' \
     "$arg"
 done
